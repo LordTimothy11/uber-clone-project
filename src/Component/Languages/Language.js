79f2f20576ea7cf53344cff21377947d0a89ff63
Samuel Timothy <lordtimothy11@gmail.com>
2023-05-27 13:19:41 +0100

    language page

--- src/Component/Languages/Language.js
+++ src/Component/Languages/Language.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./Language.css";
 import Navbar from "../Navbar/Navbar";
+import { IoCloseSharp } from "react-icons/io5";
+import { Link } from "react-router-dom";
 
 
 
@@ -8,12 +10,23 @@ function Language(){
     return(
         <div className="language">
             <Navbar />
+            <div className="language_icon">
+                <Link to="/" style={{textDecoration:"none"}}>
+                <IoCloseSharp className="language_top_ioclose"/>
+                </Link>
+                </div>
             <div className="language_one">
             <div className="language_up">
             <p>Select your preferred language</p>
             </div>
                 <div className="language_down">
-                    <p>English</p>
+                    <select>
+                    <option>English</option>
+                    <option>French</option>
+                    <option>Spanish</option>
+                    <option>Dutch</option>
+                    <option>German</option>
+                    </select>
                 </div>
             </div>
         </div>
